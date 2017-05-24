7b7cec5c145bc46d9418ab37351fd81911f92798
Kris Watson <kris@computestacks.com>
2017-05-23 20:23:00 -0700

    Fix SSO

--- lib/whmcs/sso.rb
+++ lib/whmcs/sso.rb
@@ -3,7 +3,7 @@ module Whmcs
 
     def self.auth_user!(email, password)
       client = Whmcs::Client.new
-      raise Whmcs::NotImplemented, 'SSO is disabled.' unless Whmcs.config.enable_sso
+      raise Whmcs::NotImplemented, 'SSO is disabled.' unless Whmcs.config[:enable_sso]
       raise Whmcs::MissingParameter, "Missing required parameters" if client.nil? || email.nil? || password.nil?
       data = {
         email: email,
